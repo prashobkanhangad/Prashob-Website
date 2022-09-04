02dc118fd5b48132956c2e9b81d7cd7ae4817242
prashobkanhangad <info.prashob@gmail.com>
2022-09-04 16:46:49 +0530

    first commit

---
+++ validate.js
@@ -0,0 +1,33 @@
+
+// function validate() {
+//     if (document.f1.name.value === "") {
+//         document.getElementById("pname").innerHTML = "*fill the form";
+//         document.getElementById('inpn').focus;
+//         document.getElementById('inpn').style.borderColor = 'red'
+//         return false;
+//     }
+
+//     else if (!isNaN(document.f1.email.value)) {
+//         document.getElementById("pname").innerHTML = "*only text";
+//         document.getElementById('inpn').focus;
+//         document.getElementById('inpn').style.borderColor = 'red'
+//         return false;
+//     }
+
+//     else if (document.f1.subject.value === "") {
+//         document.getElementById("pemail").innerHTML = "*fill the form";
+//         document.getElementById('inpe').focus;
+//         document.getElementById('inpe').style.borderColor = 'red'
+//         return false;
+//     }
+
+
+//     else if (document.f1.message.value === "") {
+//         document.getElementById("parea").innerHTML = "*fill the form";
+//         document.getElementById('inpa').focus;
+//         document.getElementById('inpa').style.borderColor = 'red'
+//         return false;
+//     }
+
+// }
+
